71e10172412af323efb2b4afedf5b53bde8c745e
Carl0017 <smsmartgrowth@gmail.com>
2025-01-29 13:56:47 -0800

    Campaign page header done

--- src/pages/Campaigns/App.js
+++ src/pages/Campaigns/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
 
 import Navbar from "../../components/Navbar";
-//import "./About.scss";
+import "./App.scss";
 import Footer from "../../components/Footer";
 
 function App() {
   return (
-    <div>Campaings</div>
+    <div className="Campaigns">
+        <div className="overlay"></div>
+
+        <div className="content">
+          <Navbar />
+
+          <div>
+            <h1>Campaigns</h1>
+          </div>
+
+          <div className="extra">
+            <br></br>
+          </div>
+        </div>
+      </div>
   )
 }
 
